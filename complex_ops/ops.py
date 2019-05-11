56932fa87b793e180f784342631acd1b223c944a
Patrik Ohlsson <patrik.ohlsson@doremir.com>
2019-05-11 20:17:01 +0200

    Fix

--- complex_ops/ops.py
+++ complex_ops/ops.py
@@ -2,6 +2,7 @@ import tensorflow as tf
 import numpy as np
 
 ke = tf.keras
+K = ke.backend
 
 
 def dense(x, dim, norm=False, actf=None, name=None, training=True):
@@ -56,11 +57,53 @@ def dft_matrix(n_fft):
 def calculate_frames(duration, window_length, hop_length):
     return int(np.ceil((duration - window_length) / hop_length))
 
+class CFC(ke.layers.Layer):
+    def __init__(self, units, **kwargs):
+        self.fc = ke.layers.Dense(units * 2, **kwargs)
+        super().__init__()
+
+    def build(self, input_shape):
+        self.fc.build(input_shape[:-1].as_list() + [input_shape[-1].value * 2])
+        self._trainable_weights += self.fc.trainable_weights
+        super().build(input_shape)
+
+    def call(self, x):
+        if x.dtype != 'complex64':
+            x = tf.concat([x, tf.zeros_like(x)], axis=-1)
+        else:
+            x = tf.concat([tf.real(x), tf.imag(x)], axis=-1)
+        y = self.fc(x)
+        yr, yi = tf.split(y, 2, axis=-1)
+        return tf.complex(yr, yi)
+
+    def compute_output_shape(self, input_shape):
+        return input_shape[:-1] + (self.fc.units // 2,)
+
+class iCFC(ke.layers.Layer):
+    def __init__(self, fc, **kwargs):
+        self.fc = fc
+        super().__init__(**kwargs)
+
+    def build(self, input_shape):
+        super().build(input_shape)
+
+    def call(self, x):
+        if x.dtype != 'complex64':
+            x = tf.concat([x, tf.zeros_like(x)], axis=-1)
+        else:
+            x = tf.concat([tf.real(x), tf.imag(x)], axis=-1)
+        k = self.fc.fc.kernel
+        k = tf.matmul(tf.matrix_inverse(tf.matmul(k, k, transpose_a=True)), k, transpose_b=True)
+        y = tf.matmul(x, k)
+        yr, yi = tf.split(y, 2, axis=-1)
+        return tf.complex(yr, yi)
+
+    def compute_output_shape(self, input_shape):
+        return input_shape[:-1] + (self.fc.fc.kernel.shape[0].value,)
 
 class CDense(ke.layers.Layer):
-    def __init__(self, output_dim, real_input=False, polar_mode=False, use_bias=True, kernel_initializer=None, **kwargs):
+    def __init__(self, output_dim, polar_mode=False, use_bias=True, kernel_initializer=None, **kwargs):
         self.output_dim = output_dim
-        self.real_input = real_input
         self.polar_mode = polar_mode
         self.use_bias = use_bias
         self.kernel_initializer = kernel_initializer
@@ -75,8 +118,9 @@ class CDense(ke.layers.Layer):
                 real_initializer = self.kernel_initializer
                 imag_initializer = self.kernel_initializer
             else:
-                real_initializer = ke.initializers.VarianceScaling()
-                imag_initializer = ke.initializers.VarianceScaling()
+                scale = tf.rsqrt(float(self.output_dim))
+                real_initializer = ke.initializers.RandomNormal(0., scale)
+                imag_initializer = ke.initializers.RandomNormal(0., scale)
         self.real_kernel = self.add_weight(name='real_kernel',
                                            shape=(
                                                input_shape[-1].value, self.output_dim),
@@ -103,7 +147,7 @@ class CDense(ke.layers.Layer):
         super(CDense, self).build(input_shape)
 
     def call(self, x):
-        if self.real_input:
+        if x.dtype != tf.complex64:
             x = tf.complex(x, 0.)
         if self.polar_mode:
             w = tf.complex(self.real_kernel, 0.) * \
@@ -125,8 +169,6 @@ class CDense(ke.layers.Layer):
     def get_config(self):
         config = {
             'output_dim': self.output_dim,
-            'output_dim': self.output_dim,
-            'real_input': self.real_input,
             'polar_mode': self.polar_mode,
             'use_bias': self.use_bias,
             'kernel_initializer': self.kernel_initializer
@@ -134,10 +176,31 @@ class CDense(ke.layers.Layer):
         base_config = super(CDense, self).get_config()
         return dict(list(base_config.items()) + list(config.items()))
 
+class iCDense(ke.layers.Layer):
+    def __init__(self, fc, **kwargs):
+        self.fc = fc
+        super().__init__(**kwargs)
+
+    def build(self, input_shape):
+        super().build(input_shape)
+    
+    def _invert_kernel(self, k):
+        return tf.matmul(tf.matrix_inverse(tf.matmul(k, k, transpose_a=True)), k, transpose_b=True)
+
+    def call(self, x):
+        kr = self._invert_kernel(self.fc.real_kernel)
+        ki = self._invert_kernel(self.fc.imag_kernel)
+        k = tf.complex(kr, ki)
+        if self.fc.use_bias:
+            x = x - tf.complex(self.fc.real_bias, self.fc.imag_bias)
+        return tf.tensordot(x, k, [[-1], [0]])
+
+    def compute_output_shape(self, input_shape):
+        return input_shape[:-1] + (self.fc.real_kernel.shape[0].value,)
 
 class CActivation(ke.layers.Layer):
     def __init__(self, activation, use_magnitude=False, **kwargs):
-        self.activation = activation
+        self.activation = ke.activations.get(activation)
         self.use_magnitude = use_magnitude
         super(CActivation, self).__init__(**kwargs)
 
@@ -153,6 +216,101 @@ class CActivation(ke.layers.Layer):
         else:
             y = tf.complex(self.activation(tf.real(x)),
                            self.activation(tf.imag(x)))
+        return y
+
+    def compute_output_shape(self, input_shape):
+        return input_shape
+
+class CFreqDense(ke.layers.Layer):
+    def __init__(self, output_dim, **kwargs):
+        super().__init__(**kwargs)
+        self.output_dim = output_dim
+
+    def build(self, input_shape):
+        input_dim = input_shape[-1].value
+        a = self.add_weight(
+            'kernel',
+            shape=(1, self.output_dim),
+            dtype=tf.float32,
+            initializer=ke.initializers.RandomNormal(0., 1.)
+        )
+        f = self.add_weight(
+            'freqs',
+            shape=(1, self.output_dim),
+            dtype=tf.float32,
+            initializer=ke.initializers.RandomNormal(0., 1.)
+        )
+
+        i = tf.reshape(tf.range(input_dim, dtype=tf.float32), [-1, 1])
+
+        a = tf.nn.softmax(a, axis=-1)
+        j = tf.nn.sigmoid(f)
+
+        self.W = tf.complex(a, 0.) * tf.exp(tf.complex(0., 2 * np.pi * i * j))
+        super().build(input_shape)
+
+    def call(self, x):
+        if x.dtype != 'complex64':
+            x = tf.complex(x, 0.)
+        y = tf.tensordot(x, self.W, [[-1], [0]])# / tf.complex(tf.sqrt(float(self.output_dim)), 0.)
+        return y
+
+    def compute_output_shape(self, input_shape):
+        return input_shape[:-1] + (self.output_dim,)
+
+class CLayerNormalization(ke.layers.Layer):
+    def __init__(self, use_magnitude=False, **kwargs):
+        self.use_magnitude = use_magnitude
+        self.eps = 1e-6
+        super().__init__(**kwargs)
+
+    def build(self, input_shape):
+        dim = input_shape[-1].value
+        self.gamma_real = self.add_weight(
+            'gamma_real',
+            (dim,),
+            dtype=tf.float32,
+            initializer='ones'
+        )
+        if not self.use_magnitude:
+            self.gamma_imag = self.add_weight(
+                'gamma_real',
+                (dim,),
+                dtype=tf.float32,
+                initializer='ones'
+            )
+            
+            self.beta_real = self.add_weight(
+                'beta_real',
+                (dim,),
+                dtype=tf.float32,
+                initializer='zeros'
+            )
+
+            self.beta_imag = self.add_weight(
+                'beta_imag',
+                (dim,),
+                dtype=tf.float32,
+                initializer='zeros'
+            )
+
+        super().build(input_shape)
+
+    def call(self, x, training=None):
+        if x.dtype != 'complex64':
+            x = tf.complex(x, 0.)
+        if self.use_magnitude:
+            y_mag = tf.abs(x)
+            y_mean = K.mean(y_mag, axis=0, keepdims=True)
+            y_std = K.std(y_mag, axis=0, keepdims=True)
+            
+            y_norm = y_mag / (y_std + self.eps)
+            y = tf.complex(self.gamma_real * y_norm, 0.) * tf.exp(tf.complex(0., tf.angle(x)))
+        else:
+            y_mean = K.mean(x, axis=-1, keepdims=True)
+            y_std = K.std(x, axis=-1, keepdims=True)
+            y = tf.complex(self.gamma_real, self.gamma_imag) * (x - y_mean) / y_std + tf.complex(self.beta_real, self.beta_imag)
+
         return y
 
     def compute_output_shape(self, input_shape):
